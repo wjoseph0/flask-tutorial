2cbf756d88f547b15e74d57bd011301f32793498
Joseph Wagner <68037585+wjoseph0@users.noreply.github.com>
2024-05-16 20:44:28 -0500

    Register & Login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -4,3 +4,72 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET'))
+def register_get():
+    return render_template('auth/register.html')
+
+@bp.route('/register', methods=('POST'))
+def register_post():
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
+
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+
+    if error is None:
+        try:
+            db:execute(
+                "INSERT INTO user (username, password) VALUES (?,?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+        else:
+            return redirect(url_for("auth.login"))
+
+    flash(error)
+    return render_template('auth/register.html')
+
+@bp.route('login', methods=('GET'))
+def login_get():
+    return render_template('auth/login.html')
+
+@bp.route('login', methods=('POST'))
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
+    user = db.execute(
+        'SELECT * FROM user WHERE username = ?', (username,)
+    ).fetchone()
+
+    if user is None:
+        error = 'Incorrect username.'
+    elif not check_password_hash(user['password'], password):
+        error = 'Incorrect password.'
+    
+    if error is None:
+        session.clear()
+        session['user_id'] = user['id']
+        return redirect(url_for('index'))
+    
+    flash(error)
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
